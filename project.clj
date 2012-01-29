22fd5d326f37c9c5ed9d6cec5ac3e1a83f4e7be6
Sam Ritchie <sritchie09@gmail.com>
2012-01-29 12:28:28 -0800

    update gitignore.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject enlive "paddleguru/1.2.0-alpha1"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2"]])
